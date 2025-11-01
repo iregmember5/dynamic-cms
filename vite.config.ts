5dfd6ea8baeaf7a14ae9eee7cf694f75d7fd4fcd
dawood <facultystudent05@gmail.com>
2025-11-01 12:55:58 +0500

    First Commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/blogs': {
+      '/blogs/api': {
         target: 'https://esign-admin.signmary.com',
         changeOrigin: true,
         secure: false,
+         headers: {
+          "X-Frontend-Url": "https://dynamic-cms-zeta.vercel.app/",
+        },
         rewrite: (path) => path,
       }
     }
