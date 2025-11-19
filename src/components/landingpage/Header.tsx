5524058371e96a6c541e5cb373eb172858ecd5af
dev1IREG61 <ireg.member61@gmail.com>
2025-11-19 16:34:10 +0500

    N

--- src/components/landingpage/Header.tsx
+++ src/components/landingpage/Header.tsx
@@ -80,7 +80,7 @@ const Header: React.FC<HeaderProps> = ({ data, onShowLogin }) => {
       </div>
 
       {/* Content Container */}
-      <div className="relative z-10 container mx-auto px-4 sm:px-6 lg:px-8 py-16">
+      <div className="relative z-10 container mx-auto px-4 sm:px-6 lg:px-4 py-16">
         <div className="flex flex-col lg:flex-row items-center justify-between gap-8 lg:gap-16">
           {/* Text Content - Left Side - Made smaller */}
           <div className="lg:w-1/2 text-center lg:text-left">
