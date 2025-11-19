0d9fc6cc10f2b8912496394b15193347abeef9a9
dev1IREG61 <ireg.member61@gmail.com>
2025-11-19 13:06:32 +0500

    New design

--- src/components/landingpage/Benefits.tsx
+++ src/components/landingpage/Benefits.tsx
@@ -142,7 +142,7 @@ const Benefits: React.FC<BenefitsProps> = ({ data }) => {
         </div>
 
         {/* Benefits Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 max-w-6xl mx-auto">
+        <div className="ggrid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6 lg:gap-8 max-w-6xl mx-auto">
           {displayBenefits.map((benefit: Benefit) => (
             <div key={benefit.id} className="group relative">
               {/* Card */}
